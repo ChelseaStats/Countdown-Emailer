c47c558ef0eed4703c4abd63f18daf5afe558c94
JoeTheAverage <billingsleyjoseph@gmail.com>
2015-06-22 22:38:37 +0100

    Refactored to export methods.

--- RSS-Feed.js
+++ RSS-Feed.js
@@ -1,56 +1,67 @@
+var Config = require('./config.js');
+var Puzzles = require('./PuzzleRepo.js');
+
+module.exports = {
+	writeFeed: function () {
+
+		var feed = getFeed();
+
+		Puzzles.getAll(function (err, puzzles) {
+			if (err) {
+				console.log(err);
+			} else {
+				puzzles.forEach(function (puzzle) {
+					feed.addItem({
+						title: "Today you must reach " + puzzle.target + "...",
+						description: "Reach " + puzzle.target + " with the numbers " + puzzle.numbers
+					});
+				});
+
+				writeRSSOut(feed);
+			}
+		});
+	}
+};
+
 var Feed = require('feed');
 
-var feed = new Feed({
-	title: "Countdown Number Puzzle",
-	description: "Your daily Countdown numbers puzzle!",
-	link: "http://countdown.rocks/",
-	author: [
-		{
-			name: "Joseph Billingsley",
-			email: "jcb@josephbillingsley.co.uk",
-			link: "http://www.josephbillingsley.co.uk"
-		},
-		{
-			name: "ChelseaStats",
-			email: "countdown@thechels.uk",
-			link: "http://www.thechels.uk"
-		}
-	]
-});
+function getFeed() {
+	var feed = new Feed({
+		title: "Countdown Number Puzzle",
+		description: "Your daily Countdown numbers puzzle!",
+		link: "http://countdown.rocks/",
+		author: [
+			{
+				name: "Joseph Billingsley",
+				email: "jcb@josephbillingsley.co.uk",
+				link: "http://www.josephbillingsley.co.uk"
+			},
+			{
+				name: "ChelseaStats",
+				email: "cde@thechels.uk",
+				link: "http://www.thechels.co.uk"
+			}
+		]
+	});
 
-feed.addCategory("Daily");
-feed.addCategory("Countdown");
-feed.addCategory("Numbers");
-feed.addCategory("Puzzle");
+	feed.addCategory("Daily");
+	feed.addCategory("Countdown");
+	feed.addCategory("Numbers");
+	feed.addCategory("Puzzle");
 
-var Puzzles = require('PuzzlesRepo');
-
-Puzzles.findPosts(function (err, puzzles) {
-	if (err) {
-
-	} else {
-		puzzles.forEach(function (puzzle) {
-			feed.item({
-				title: "Today you must reach " + puzzle.target,
-				description: "With the numbers " + puzzle.numbers
-			});
-		});
-
-		writeRSSOut(feed);
-	}
-});
+	return feed;
+}
 
 function writeRSSOut(feed) {
-	var rss = feed.render('rss-2.0');
 
 	var fs = require('fs');
-	var config = require('./config.js');
+	var rss = feed.render('rss-2.0');
 	
-	fs.writeFile(config.rssLoc, rss, function (err) {
+	fs.writeFile(Config.rssDest, rss, function (err) {
 		if (err) {
 			return console.log(err);
 		}
-		
+
 		console.log("The RSS feed was updated succesfully!");
 	});
 }
